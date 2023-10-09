4b9b59e69562c09eea18a1bda8c2c0a5b2c1cff0
IchiruChiru <richiejonathanchaniago@gmail.com>
2023-10-09 13:12:17 +0700

    route

---
+++ resources/views/profil.blade.php
@@ -0,0 +1,1 @@
+<h1>Hallo, Nama saya Richie Jonathan Chaniago</h1>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,38 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/profil', function () {
+    return view('profil');
+});
+
+Route::get('/mahasiswa/{nama}', function ($nama = "Richie") {
+    echo "<h2>Halo Nama Saya $nama</h2>";
+});
+
+Route::get('/mahasiswa2/{nama?}', function ($nama = "Richie") {
+    echo "<h2>Halo Nama Saya $nama</h2>";
+});
+
+Route::get('/profil/{nama?}/{pekerjaan?}', function ($nama = "Richie", $pekerjaan = "Mahasiswa") {
+    echo "<h2>Halo Nama Saya $nama. Saya adalah $pekerjaan</h2>";
+});
+
+Route::get('/hubungi', function () {
+    echo "<h1>Hubungi Kami</h1>";
+})->name("call");
+
+route::redirect('/contact', '/hubungi');
+
+route::get('/halo', function () {
+    echo "<a href='" . route('call') . "'>" . route('call') . "</a>";
+});
+
+route::prefix('/dosen')->group(function () {
+    route::get('/jadwal', function () {
+        echo "<h1>Jadwal Dosen</h1>";
+    });
+    route::get('/materi', function () {
+        echo "<h1>materi Dosen</h1>";
+    });
+});
